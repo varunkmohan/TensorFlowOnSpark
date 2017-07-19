0d52e81ad8d53121355a224daad7c6febad37cc5
Varun Mohan <varun.mohan@databricks.com>
2017-07-19 13:38:59 -0700

    typo

--- tensorflowonspark/util.py
+++ tensorflowonspark/util.py
@@ -1,3 +1,10 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import nested_scopes
+from __future__ import print_function
+
+import socket
+
 def get_ip_address():
   s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
   s.connect(("8.8.8.8", 80))
